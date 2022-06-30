d370a3f33cfba9516e2a9a2234446355a3187bf3
leonardo-terra <leo.terra.melo@gmail.com>
2022-06-30 19:02:46 -0300

    feat: configura variaveis de ambiente

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -15,6 +15,7 @@ const upsideDownConfig = {
   url: process.env.REACT_APP_UPSIDEDOWN_URL,
   timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
+//:
 
 const charactersService = new CharactersService(strangerThingsConfig);
 const charactersUpsideDownService = new CharactersService(upsideDownConfig);
